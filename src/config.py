e7117f856ffbfbfed9b17d60afaa68af9fcd5d0d
Matthew Mascioni <mascionim@gmail.com>
2021-01-16 19:17:02 -0500

    added back support for running without docker

--- src/config.py
+++ src/config.py
@@ -8,7 +8,17 @@ from dotenv import find_dotenv, load_dotenv
 
 class Config(object):
     """Base configuration all other configurations inherit from."""
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    db_string = 'postgresql://{user}:{password}@{host}/{db}'.format(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST', 'localhost'),
+        db=os.getenv('DB_DATABASE')
+    )
+
+    # Try to find a DATABASE_URL environment variable. If none is found,
+    # fall back to one assembled from DB_USER, _PASSWORD, _HOST and _DATABASE
+    # env vars:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', default=db_string)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
