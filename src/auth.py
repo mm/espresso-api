e7e06a87aa14c8db989a3dd18c0a28b528f7127f
Matthew Mascioni <mascionim@gmail.com>
2021-01-31 11:52:26 -0500

    auth0 scaffold for verifying JWTs, will probably switch to Firebase after though

--- src/auth.py
+++ src/auth.py
@@ -6,7 +6,10 @@ from functools import wraps
 from typing import NamedTuple
 from secrets import token_hex, compare_digest
 from hashlib import sha256
-from flask import request, jsonify
+import os
+import requests
+from flask import request, jsonify, current_app
+from jose import jwt
 from src.model import User
 
 api_pair = NamedTuple('KeyDetails', [('api_key', str), ('hashed_key', str)])
@@ -18,6 +21,77 @@ class AuthError(Exception):
     def __init__(self, message, status_code=401):
         self.message = message
         self.status_code = status_code
+
+
+class AuthService:
+    """Service used to interact with Auth0, generate or validate
+    API keys for a given user.
+    """
+
+    def __init__(self):
+        self.auth0_domain = os.getenv('AUTH0_DOMAIN')
+        self.auth0_audience = os.getenv('AUTH0_API_AUDIENCE')
+    
+        if (not self.auth0_domain) or (not self.auth0_audience):
+            current_app.logger.error("Auth0 domain and audience missing")
+            raise AuthError("Authentication service not available")
+
+    
+    def _get_auth_token_header(self, bearer_token: str) -> str:
+        """From a token like `Bearer _______`, pulls out the _____
+        part to extract the token itself (and check if we're in the
+        right format to begin with)
+        """
+        parts = bearer_token.split()
+
+        if (parts[0].lower() != "bearer") or (len(parts) == 1) or (len(parts) > 2):
+            raise AuthError("Authorization token given in the incorrect format")
+    
+        return parts[1]
+
+
+    def _validate_jwt(self, token: str) -> User:
+        """Validates a JWT against the JSON Web Key Set hosted
+        on Auth0.
+        """
+        
+        jwks = None
+        try:
+            jwks = requests.get(
+                f'https://{self.auth0_domain}/.well-known/jwks.json'
+            ).json()
+        except Exception as e:
+            current_app.logger.error(f"Could not fetch JWKS: {e}")
+            raise AuthError("Could not validate against JSON Web Key Set")
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=['RS256'],
+                    audience=self.auth0_audience,
+                    issuer=f'https://{self.auth0_domain}/'
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError("Authorization token expired")
+            except jwt.JWTClaimsError:
+                raise AuthError("Incorrect claims")
+            except Exception:
+                raise AuthError("Authorization failed")
+            print(payload)
+        else:
+            raise AuthError("Unable to find required key")
 
 
 def generate_api_key() -> api_pair:
@@ -73,11 +147,14 @@ def user_for_api_key(api_key: str) -> User:
     return None
 
 
+
 def user_from_jwt(authorization_header):
     """Attempts to validate a JWT and retrieve the user from the JWT
     claims. Returns a User object if authentication was successful.
     """
-    return None
+    auth = AuthService()
+    token = auth._get_auth_token_header(authorization_header)
+    auth._validate_jwt(token)
 
 
 def requires_auth(f):
