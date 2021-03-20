3df2d456dac014bd47b4a5475328daa7d201a361
Matthew Mascioni <mascionim@gmail.com>
2021-03-20 17:31:46 -0400

    adds delete and get many tests for link service

--- tests/test_link_service.py
+++ tests/test_link_service.py
@@ -52,3 +52,38 @@ def test_update_link_no_id_overwrite(scoped_app):
     }
     LinkService().update_link(link, changes)
     assert link.id == ORIGINAL_ID
+
+
+def test_delete_link(scoped_app):
+    link = LinkFactory()
+    # Persist it to the database
+    LinkService().create_link(link)
+    link_id = int(link.id)
+    # Delete it:
+    LinkService().delete_link(link)
+
+    # When you try accessing it again, NotFound should be raised:
+    with pytest.raises(NotFound):
+        LinkService().get_link(link_id)
+
+
+@pytest.mark.parametrize("show_param", ['all', 'read', 'unread'])
+def test_get_links_show_param(scoped_app, show_param):
+    """The 'show' parameter should control whether all, only read or
+    only unread links are shown in the API response.
+    """
+    # Create a user and 100 links beneath them:
+    user = UserFactory()
+    links = LinkFactory.build_batch(100, user=user)
+    link_service = LinkService()
+
+    params = {'page': 1, 'per_page': 50, 'show': show_param}
+
+    response = link_service.get_many_links(user.id, params)
+    for link in response['links']:
+        if show_param == 'all':
+            assert (link.read) or (not link.read)
+        elif show_param == 'read':
+            assert link.read
+        elif show_param == 'unread':
+            assert not link.read
