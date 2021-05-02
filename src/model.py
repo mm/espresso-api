aa08465b0ba094faef9a7f6edeb70e205be7fdad
Matthew Mascioni <mascionim@gmail.com>
2021-05-02 12:50:56 -0400

    fixed psycopg2 issues and added description column for links

--- src/model.py
+++ src/model.py
@@ -66,6 +66,7 @@ class Link(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     title = db.Column(db.String(512), nullable=True)
     read = db.Column(db.Boolean, nullable=False, default=False)
+    description = db.Column(db.String)
 
     def __repr__(self):
         return "<Link: {} [{}]>".format(self.url, self.id)
